2ce5bce4bb0fb4f6596a41dd4ed6642c3fd0c3f1
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2026-03-28 09:50:22 +0530

    navbar ui update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,8 +4,14 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <nav className="navbar">
+      <Link to="/" className="nav-brand">
+        <div className="nav-brand-icon">👾</div>
+        <span className="nav-brand-name">Arcade Hub</span>
+      </Link>
+
       <Link to="/" className="nav-home">
-        ← Home
+        <span className="nav-home-arrow">←</span>
+        Home
       </Link>
     </nav>
   );
